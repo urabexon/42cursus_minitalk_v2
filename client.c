7fe7b816885a695e1257506d4337d83ec013d4f3
Hiroki Urabe <hurabe@c2r6s8.42tokyo.jp>
2024-09-01 19:01:29 +0900

    finish

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 16:36:12 by hurabe            #+#    #+#             */
-/*   Updated: 2024/09/01 16:09:07 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/09/01 18:55:22 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,6 @@
 #include "libft/libft.h"
 
 bool	g_acknowledgement = false;
-
-//static void	handle_check(int pid)
-//{
-//	int	i;
-
-//	i = 8;
-//	//while (i--)
-//	//{
-//	//	kill(pid, SIGUSR1);
-//	//	usleep(100);
-//	//}
-//	//printf("%d\n",sig);
-//	//if (sig == SIGUSR1)
-//	//	g_acknowledgement = true;
-//	//ft_printf("Client, Finish!");
-//}
 
 static void	ft_error(int error)
 {
@@ -42,11 +26,8 @@ static void	ft_error(int error)
 	exit(EXIT_FAILURE);
 }
 
-//static void	send_signal(int s_pid, char c)
 static void	send_signal(int s_pid, char *s)
 {
-	//int	sig;
-	//int	i;
 	int		i;
 	char	c;
 
@@ -56,55 +37,23 @@ static void	send_signal(int s_pid, char *s)
 		c = *s++;
 		while (i--)
 		{
-			//if (c & (1 << i))
 			if (c >> i & 1)
 			{
 				if (kill(s_pid, SIGUSR2) == -1)
 					ft_error(KILL_ERROR);
-				}
+			}
 			else
 			{
 				if (kill(s_pid, SIGUSR1) == -1)
 					ft_error(KILL_ERROR);
 			}
-			//	sig = SIGUSR1;
-			//g_acknowledgement = false;
-			//if (kill(s_pid, sig) == -1)
-			//	ft_error(KILL_ERROR);
-			//while (g_acknowledgement == false)
-			//{
-			//	printf("test");
-			//	usleep(50);
-			//}
-			//while (kill(s_pid, sig) == -1)
 			g_acknowledgement = false;
 			usleep(3500);
 		}
-		//if (*s == '\0')
 	}
 	ft_printf("client finish!\n");
 	exit(0);
 }
-
-//static int	check_pid(char *c_pid)
-//{
-//	int	i;
-//	int	len;
-//	int	i_pid;
-
-//	len = ft_strlen(c_pid);
-//	i = 0;
-//	while (i < len)
-//	{
-//		if (!ft_isdigit(c_pid[i]))
-//			ft_error(PID_ERROR);
-//		i++;
-//	}
-//	i_pid = ft_atoi(c_pid);
-//	if (i_pid <= 1)
-//		ft_error(PID_ERROR);
-//	return (i_pid);
-//}
 
 int	main(int argc, char **argv)
 {
@@ -113,11 +62,7 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 		ft_error(INPUT_ERROR);
 	i_pid = ft_atoi(argv[1]);
-	//signal(SIGUSR1, handle_check);
-	//signal(SIGUSR2, handle_check);
 	send_signal(i_pid, argv[2]);
 	send_signal(i_pid, "\0");
-	//while (1)
-	//pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 16:36:20 by hurabe            #+#    #+#             */
-/*   Updated: 2024/09/01 16:04:41 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/09/01 18:57:31 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ static void	signal_handler(int sig, siginfo_t *info, void *context)
 	static unsigned char	c = 0;
 
 	(void)context;
-	//(void)info;
 	if (!client_pid)
 		client_pid = info->si_pid;
 	c |= (sig == SIGUSR2);
@@ -34,68 +33,16 @@ static void	signal_handler(int sig, siginfo_t *info, void *context)
 		}
 		ft_putchar_fd(c, 1);
 		c = 0;
-		////kill(client_pid, SIGUSR1);
 	}
 	else
 		c <<= 1;
 }
-//static void	signal_handler(int sig, siginfo_t *info, void *context)
-//{
-//	static int		c_pid = 0;
-//	static unsigned char	c = 0;
-//	static int		i = 0;
-
-//	(void)context;
-//	if (c_pid == 0)
-//		c_pid = info->si_pid;
-//	if (sig == SIGUSR2)
-//		c |= (1 << i);
-//	i++;
-//	if (i == 8)
-//	{
-//		write(1, &c, 1);
-//		i = 0;
-//		c = 0;
-//		if (c == '\0')
-//			c_pid = 0;
-//	}
-//	kill(c_pid, SIGUSR1);
-//}
-
-//static void	signal_handler(int sig, siginfo_t *info, void *context)
-//{
-//	static int		c_pid;
-//	static char		c;
-//	static int		i;
-
-//	(void)context;
-//	if (!c_pid)
-//		c_pid = info->si_pid;
-//	if (sig == SIGUSR2)
-//		c |= (1 << i);
-//	i++;
-//		kill(c_pid, SIGUSR1);
-//	if (i == 8)
-//	{
-//		write(1, &c, 1);
-//		i = 0;
-//		if (c == '\0')
-//		{
-//			//kill(c_pid, SIGUSR1);
-//			c_pid = 0;
-//		}
-//		c = 0;
-//		//write(1, "\n", 1);
-//	}
-//	return ;
-//}
 
 int	main(void)
 {
 	struct sigaction	sa;
 
 	ft_printf("PID: %d\n", getpid());
-
 	sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = signal_handler;
 	sa.sa_flags = SA_SIGINFO;
